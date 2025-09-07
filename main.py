2fec2df9ca851c69e934d7a5dff025eb8c827c2a
ThePrimeagen <the.primeagen@aol.com>
2025-09-07 10:22:34 -0500

    FPS limit added

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 1 == 1:
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    pygame.screen.fill("black")
-    pygame.screen.flip()
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
